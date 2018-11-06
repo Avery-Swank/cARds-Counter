0edde579a147f9586d349764d3c4903111182736
Avery-Swank <swankerzzzz@gmail.com>
2018-11-06 16:21:52 -0600

    Initial Unity upload with working AR interactions with playing cards and working running count counter

---
+++ Assets/ImageTargetAddCount.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ImageTargetAddCount : MonoBehaviour, ITrackableEventHandler {
+
+    private TrackableBehaviour m_behavior;
+
+    public int m_countValue;
+
+    void Start()
+    {
+        m_behavior = GetComponent<TrackableBehaviour>();
+
+        // Register if found
+        if (m_behavior)
+            m_behavior.RegisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        // Update running count when an image is found
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            RunningCount.PlusValue(m_countValue);
+        }
+    }
+}
---
+++ Assets/RunningCount.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RunningCount : MonoBehaviour {
+
+    private string m_description = "Running Count: ";
+    private Text m_descriptionText;
+
+    public static int m_count;
+
+	void Start () {
+        m_descriptionText = GetComponent<Text>();
+        m_count = 0;
+	}
+	
+	void Update () {
+        m_descriptionText.text = m_description + m_count;
+	}
+
+    public static void PlusValue(int value)
+    {
+        m_count += value;
+    }
+}
